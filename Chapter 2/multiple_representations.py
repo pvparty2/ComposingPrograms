2b601d166e067f0b313b73b9e63df6146c1165b5
Petro Partyka <petrovpartyka@gmail.com>
2021-08-29 11:28:06 -0500

    Progress through book and reddit challenge

--- Chapter 2/multiple_representations.py
+++ Chapter 2/multiple_representations.py
@@ -56,3 +56,70 @@ class ComplexMA(Complex):
     def __repr__(self):
         return f'ComplexMA({self.magnitude}, {self.angle/pi} * pi)'
     
+
+# Generic Functions
+from math import gcd
+class Rational(Number):
+    def __init__(self, numer, denom):
+        g = gcd(numer, denom)
+        self.numer = numer // g
+        self.denom = denom // g
+    def __repr__(self):
+        return f'Rational ({self.numer}, {self.denom})'
+    def add(self, other):
+        nx, dx = self.numer, self.denom
+        ny, dy = other.numer, other.denom
+        return Rational(nx * dy + ny * dx, dx * dy)
+    def mul(self, other):
+        numer = self.numer * other.numer
+        denom = self.denom * other.denom
+        return Rational(numer, denom)
+
+# Type Dispatching
+def is_real(c):
+    '''Return whether c is a real number with no imaginary part.'''
+    if isinstance(c, ComplexRI):
+        return c.imag == 0
+    elif isinstance(c, ComplexMA):
+        return c.angle % pi == 0 
+
+
+def add_complex_and_rational(c, r):
+    return ComplexRI(c.real + r.numer/r.denom, c.imag)
+
+
+def mul_complex_and_rational(c, r):
+    r_magnitude, r_angle = r.numer/r.denom, 0
+    if r_magnitude < 0:
+        r_magnitude, r_angle = -r_magnitude, pi
+    return ComplexMA(c.magnitude * r_magnitude, c.angle + r_angle)
+
+
+def add_rational_and_complex(r, c):
+    return add_complex_and_rational(c, r)
+
+
+def mul_rational_and_complex(r, c):
+    return mul_complex_and_rational
+
+
+class Number:
+    def __add__(self, other):
+        if self.type_tag == other.type_tag:
+            return self.add(other)
+        elif (self.type_tag, other.type_tag) in self.adders:
+            return self.cross_apply(other, self.adders)
+    def __mul__(self, other):
+        if self.type_tag == other.type_tag:
+            return self.mul(other)
+        elif (self.type_tag, other.type_tag) in self.multipliers:
+            return self.cross_apply(other, self.multipliers)
+    def cross_apply(self, other, cross_fns):
+        cross_fn = cross_fns[(self.type_tag, other.type_tag)]
+        return cross_fn(self, other)
+    adders = {('com', 'rat'): add_complex_and_rational,
+              ('rat', 'com'): add_rational_and_complex}
+    multipliers = {('com', 'rat'): mul_complex_and_rational,
+                   ('rat', 'com'): mul_rational_and_complex}
+
+
--- challenges/enable1.py
+++ challenges/enable1.py
@@ -9,22 +9,44 @@ def letter_sum(word) -> int:
     Value of a letter is determined by its position in the alphabet.
     Ex. A = 1, B = 2, C = 3, and so on. 
     '''
-    _base = 96
+    BASE = 96
     if len(word) == 1:
-        return ord(word) - _base
-    return ord(word[0]) - _base + letter_sum(word[1:])  # My attempt at recursion
+        return ord(word) - BASE
+    return ord(word[0]) - BASE + letter_sum(word[1:])  # My attempt at recursion
 
 
-# Find the word with letter sum of 319
-def words_with_sum(words, n) -> list:
+def make_words_sums(words):
+    '''Return a dictionary of each word and its respective letter sum.'''
+    return {word: letter_sum(word) for word in words}
+
+
+# Find the word(s) with letter sum of n
+def words_with_sum_n(words:dict, n) -> list:
     '''Return a list of words that sum to n.'''
-    result = []
-    for word in words:
-        if letter_sum(word) == n:
-            result.append(word)
-    return result
+    return [k for k, v in words.items() if v == n]
 
 
+# Find the word(s) with letter sum of n
+def words_with_sum(words:list, n) -> list:
+    '''Return a list of words that sum to n.'''
+    return [w for w in words if letter_sum(w) == n]
+
+
+# Query a dictionary by it's values
+def filter_values(words:dict, my_func):
+    '''
+        Apply a function on the values to query the dictionary.
+        Return a list of the resulting values.'''
+    return [my_func(v) for v in words.values() if my_func(v) is not None]
+
+
+def filter_keys(words:dict, my_func):
+    '''
+        Apply a function on the keys to query the dictionary.
+        Return a list of the resulting keys.'''
+    return [my_func(k) for k in words.keys() if my_func(k) is not None]
+
+   
 # Find the total number of words whose letter sum is odd
 def count_odd_letter_sum(words) -> int:
     '''Return the total count of words whose letter sum is odd.'''
@@ -36,8 +58,7 @@ def count_odd_letter_sum(words) -> int:
 
 
 # Find the number of words for each available letter sums.
-# Work in Progress...
-def word_sizes(words) -> dict:
+def get_word_sizes(words:list) -> dict:
     '''Return a count of the number of words for each letter sum.'''
     a = {}
     for word in words:
@@ -48,14 +69,75 @@ def word_sizes(words) -> dict:
     return a
 
 
-with open('enable1.txt', 'r') as f:
+# Find the number of words for each available letter sums. 
+def get_word_sizes_2(words:dict) -> dict:
+    '''Return a count of the number of words for each letter sum.'''
+    _list = list(words.values())
+    _set = set(_list)
+    return {i: _list.count(i) for i in _set}
+
+
+# Sort word_sizes by converting to list of key, value pairs
+def make_pairs(dic):
+    '''Convert a dictionary to a list of tuples.'''
+    return list(dic.items())
+
+
+def sort_pairs(pairs, i, reverse=False):
+    '''Sort by the ith index of each tuple.'''
+    # Sort each tuple by ith index 
+    if reverse:
+        pairs.sort(key=lambda x: x[i], reverse=True)
+    pairs.sort(key=lambda x: x[i])
+    return pairs
+
+
+# Get the list of words
+filename = 'enable1.txt'
+with open(filename) as f:
     words = [word.strip() for word in f.readlines()]
 
-a = word_sizes(words)
+# Turn the list of words into keys with their letter sums as values
+words_sums = make_words_sums(words)
 
-highest_k = 0
-highest_v = 0
-for k, v in a.items():
-    if v > highest_v:
-        highest_v = v
-        highest_k = k
+# Question # 1: Find the only word with a letter sum of 319.
+n = 319
+filter_keys(words_sums, lambda x: x if letter_sum(x) == n else None) 
+words_with_sum_n(words_sums, n)
+words_with_sum(words, n)
+
+# Question # 2: How many words have an odd letter sum?
+len(filter_values(words_sums, lambda x: x if x % 2 != 0 else None))
+count_odd_letter_sum(words)
+
+# Question # 3: What letter sum is most common, and how many words have it?
+word_sizes = get_word_sizes_2(words_sums)
+pairs = make_pairs(word_sizes)
+pairs = sort_pairs(pairs, 1)    # Sort by y in each (x, y) pair.
+pairs[-1]   # 93 letter sum is the most common; 1965 words have it.
+
+
+# Question # 4: Find the pair of words that have the same letter sum but difference in word length of n.
+def get_same_sums(words, n):
+    '''Return a list of words with the same letter sums.'''
+    return filter_keys(words, lambda x: x if words[x] == n else None)
+
+
+def len_difference(a, b):
+    return abs(len(a) - len(b))
+
+
+set_of_sums = list(set(words_sums.values()))
+same_sums_w_difference = []
+DIFFERENCE = 11
+for _sum in set_of_sums:
+    same_sums = get_same_sums(words_sums, _sum) # create a list of words with the same letter sum
+    for i, word in enumerate(same_sums):
+        if word == same_sums[-1]:
+            break
+        for second_word in same_sums[i+1:]:
+            if len_difference(word, second_word) == DIFFERENCE:
+                same_sums_w_difference.append((word, second_word))
+
+
+
